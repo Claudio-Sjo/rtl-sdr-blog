4e38d58dae12fb0bbd5eb238f592ada2cf816b91
Claudio-Sjo <Claudio.Porfiri@gmail.com>
2026-05-06 12:26:46 +0200

    cmake: exclude full audio chain (dab-audio, dabplus, decoder_adapter, dab_decoder)

--- cmake/DabTime.cmake
+++ cmake/DabTime.cmake
@@ -8,8 +8,8 @@ if(BUILD_DAB_TIME)
     set(WELLE_DIR ${CMAKE_SOURCE_DIR}/lib/welle.io/src)
 
     file(GLOB WELLE_BACKEND_SRC "${WELLE_DIR}/backend/*.cpp")
-    # Exclude MP2 decoder chain (needs mpg123_framebyframe_decode unavailable on some systems)
-    list(FILTER WELLE_BACKEND_SRC EXCLUDE REGEX "dab_decoder|decoder_adapter")
+    # Exclude audio decoder chain (needs mpg123 API unavailable on some systems)
+    list(FILTER WELLE_BACKEND_SRC EXCLUDE REGEX "dab_decoder|decoder_adapter|dab-audio|dabplus_decoder")
     file(GLOB WELLE_VARIOUS_SRC "${WELLE_DIR}/various/*.cpp")
     set(WELLE_FEC_SRC
         ${WELLE_DIR}/libs/fec/decode_rs_char.c
