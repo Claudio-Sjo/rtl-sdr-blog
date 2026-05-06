8afb1a944d4a9d85ef5bf27460683e6d1fa3ffb6
Claudio-Sjo <Claudio.Porfiri@gmail.com>
2026-05-06 12:05:27 +0200

    cmake: only exclude dab_decoder.cpp (mpg123 issue), keep all others

--- cmake/DabTime.cmake
+++ cmake/DabTime.cmake
@@ -8,8 +8,8 @@ if(BUILD_DAB_TIME)
     set(WELLE_DIR ${CMAKE_SOURCE_DIR}/lib/welle.io/src)
 
     file(GLOB WELLE_BACKEND_SRC "${WELLE_DIR}/backend/*.cpp")
-    # Remove audio/MSC components - not needed for time extraction
-    list(FILTER WELLE_BACKEND_SRC EXCLUDE REGEX "dab_decoder|dabplus_decoder|dab-audio|decoder_adapter|msc-handler|mot_manager|pad_decoder")
+    # Only exclude dab_decoder which needs mpg123_framebyframe_decode (unavailable on some systems)
+    list(FILTER WELLE_BACKEND_SRC EXCLUDE REGEX "dab_decoder\\.cpp")
     file(GLOB WELLE_VARIOUS_SRC "${WELLE_DIR}/various/*.cpp")
     set(WELLE_FEC_SRC
         ${WELLE_DIR}/libs/fec/decode_rs_char.c
